158b608ad3aeff152ca35820f25545307bc1f79c
agusthas <suzumantan@gmail.com>
2021-06-25 20:02:51 +0700

    commits

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const Products = require('./product.json');
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var Products = require('./product.json');
+var app = express();
 
 app.get('/', (req, res) => {
-  res.send('simple rest api');
+  res.send('Simple rest api');
 });
 
-/* Routes */
-
-// GET:  all list of products
-app.get('/products/listall', (req, res) => {
+//GET all list
+app.get('/product/listall', (req, res) => {
   res.json(Products);
 });
 
-// GET: detail product by id
-app.get('/products/:id', (req, res) => {
-  const searchId = +req.params.id;
-  const product = Products.find(({ id }) => id === searchId);
-
-  res.json(product);
+//GET detail by id
+app.get('/product/:id', (req, res) => {
+  res.json(
+    Products.find((product) => {
+      return req.params.id == product.id;
+    })
+  );
 });
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+const server = app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
+
+module.exports = server;
